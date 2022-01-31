74111da002f6f19d677f1b86a392e010095632ea
user.email <adjaoud.juba.17@gmail.com>
2022-01-31 20:45:19 +0100

    Ajout du fichier player

---
+++ lib/player.py
@@ -0,0 +1,18 @@
+
+
+class Player():
+
+    def __init__(self, id, name, rank):
+        self.id = id
+        self.name = name
+        self.rank = rank
+
+    def getId(self):
+        return self.id
+
+    def getName(self):
+        return self.name
+
+    def getRank(self):
+        return self.rank    
+

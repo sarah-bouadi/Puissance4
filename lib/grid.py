faf1f83b5c2679caf62e02675dc0ea75d70ae25e
sbouadi <sarahbouadi0508@gmail.com>
2022-02-13 18:22:10 +0100

    adding grid initialisation and display

---
+++ lib/grid.py
@@ -0,0 +1,26 @@
+EMPTY_GRID = 0
+RED_PAWN = -1
+RED_WIN = -4
+YELLOW_PAWN = 1
+YELLOW_WIN = 4
+
+class grid:
+    def __init__(self, grid_size):
+        self.grid = self._initialize_grid(self.__check_grid_size(grid_size))
+        
+    def __check_grid_size(self, grid_size):
+        if(grid_size>=4):
+            return grid_size
+        else:
+            raise AttributeError("Please enter grid_size >=4 !")
+
+    def _initialize_grid(self, grid_size):
+        return [[0]*grid_size]*grid_size
+
+    def display(self):
+            print("\n")
+            for i in range(len(self.grid)):
+                for j in range(len(self.grid[i])):
+                    print(self.grid[i][j], end=' ')
+                print()
+            print("\n")

73227f3ae85db658b681f641a768ae97bc531454
soulte92 <soulte92@gmail.com>
2022-02-16 20:54:06 +0100

    Refactoring player, menu and main

--- lib/player.py
+++ lib/player.py
@@ -15,6 +15,22 @@ class Player():
     def getName(self):
         return self.name  
 
+        #Returns a tuple like: ((player1_id, player1_name), (player2_id, player2_name))
+    def initialize_P_vs_P(self):
+        player1_name = input("- Enter the name of player 1: ")
+        player1_id = input("- Enter the id of player 1: ")
+
+        player2_name = input("- Enter the name of player 2: ")
+        player2_id = input("- Enter the id of player 2: ")
+        return ((player1_id, player1_name), (player2_id, player2_name))
+
+    #Returns a tuple like: ((player1_id, player1_name), (player2_id, player2_name))
+    def initialize_P_vs_computer(self):
+        player1_name = input("- Enter the name of player 1: ")
+        player1_id = input("- Enter the id of player 1: ")
+
+        return ((player1_id, player1_name), (None, None))    
+
     def get_grid_row_from_column(self, grid, column):
         row = grid.get_size()
         while grid.matrix[row][column] != 0:
--- main.py
+++ main.py
@@ -2,5 +2,24 @@
 
 import lib.menu as sg
 
+def game_mode_menu(self):    
+    start_choice = self.input_entering("- Please enter your choice: ",[1,2,3])
+
+    #Create a new game
+    if start_choice == 1:
+        self.initialize_P_vs_P()
+    #Continue a saved game
+    elif start_choice == 2:
+        self.initialize_P_vs_computer()
+    #Quit the game
+    elif start_choice == 3:
+        self.input_quit_game()
+
+def initialize_grid(self, grid):
+    print("-****** Grid Size ******-", end='\n')
+    grid_size = input("- Please enter the grid size: ", list(range(100)))
+
+    #Initialize the grid with grid_size        
+
 if __name__ == "__main__":
     sg.start_menu()       

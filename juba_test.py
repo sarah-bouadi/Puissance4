d13618f6b4718cba12be4d1c012e5b1ea3e2405b
user.email <adjaoud.juba.17@gmail.com>
2022-02-20 01:49:12 +0100

    test

--- juba_test.py
+++ juba_test.py
@@ -1,5 +1,6 @@
 from lib.grid import *
 from lib.pawn import *
+from lib.player import *
 
 if __name__ == "__main__":
     #grille = [[None, 1, None, None],
@@ -10,7 +11,8 @@ if __name__ == "__main__":
     grille = Grid(4)
     grille.initMatrix()
     pion = Pawn(1)
+    player1 = Player("Reda", 1)
 
+    player1.add_pawn_grid(grille, 1, 2)
+    print(grille)
 
-    grille.add_pawn_grid(pion, 3)
-    grille.display()

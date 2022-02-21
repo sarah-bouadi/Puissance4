0d2b736663a2be8dbe369fe073383f85d6b144f4
sbouadi <sarahbouadi0508@gmail.com>
2022-02-21 17:43:03 +0100

    Ading isFull function

--- lib/grid.py
+++ lib/grid.py
@@ -2,11 +2,11 @@ from re import X
 from lib.pawn import *
 
 class Grid:
+
     def __init__(self, grid_size):
         """
-        Constructor of the grid
-        :param grid_size: int the size of the grid
-        :param matrix: the matrix of the grid
+        Init a grid
+        :param size: the size of the grid
         """
         self.grid_size = grid_size
         self.matrix = []
@@ -33,6 +33,14 @@ class Grid:
             return self.grid_size
         else:
             raise AttributeError("Please enter grid_size >=4 !")
+
+    def isFull(self):
+        for i in range(self.getSize()):
+            for j in range(self.getSize()):
+                if self.matrix[i][j] == None:
+                    return False
+        return True
+
 
     def _initialize_grid(self, grid_size):
         return [[None]*grid_size for i in range(grid_size)]
